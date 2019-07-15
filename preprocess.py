2e90b9b06ae651e8be809f738cf7b0f27fdfcad3
lsseong <lsseong@hotmail.com>
2019-07-15 17:40:41 +0800

    preprocessing notebook

---
+++ preprocess.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+
+
+def read_file(filename):
+    to_datetime = lambda d: datetime.strptime(d, '%d/%m/%Y %H:%M')
+    _df = pd.read_csv(filename, delimiter=";", date_parser=to_datetime, parse_dates=['date (UTC)'], index_col=0)
+    return _df
+
+
+def shape_data(df, seq_length, n_forward, sliding_step):
+    """
+    slide over time series [N] to produce training features and labels
+    :param df: time series as dataframe
+    :param seq_length:
+    :param n_forward:
+    :param sliding_step:
+    :return:
+        features: [BATCH_SIZE, SEQ_LENGTH]
+        labels: [BATCH_SIZE, N_FORWARD]
+    """
+    _data = df.to_numpy().squeeze()
+
+    batch_size = int((len(_data) - (seq_length+n_forward)) / sliding_step + 1)
+
+    _features = np.zeros((batch_size, seq_length))
+    _label = np.zeros((batch_size, n_forward))
+
+    for i in range(0, batch_size):
+        begin_index = i * sliding_step
+        stop_index = begin_index + seq_length
+        label_stop_index = stop_index + n_forward
+        _features[i] = _data[begin_index:stop_index]
+        _label[i] = _data[stop_index:label_stop_index]
+
+    return _features, _label
+
+
+def process(filename, seq_length, n_forward):
+    df = read_file(filename)
+    features, labels = shape_data(df, seq_length, n_forward, sliding_step=1)
+
+    np.savetxt("features_{}_{}.csv".format(seq_length, n_forward), features, delimiter=",", fmt="%.2f")
+    np.savetxt("labels_{}_{}.csv".format(seq_length, n_forward), labels, delimiter=",", fmt="%.2f")
+
+
+if __name__ == '__main__':
+    process('price.csv', seq_length=8, n_forward=5)
+
+
+
