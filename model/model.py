8c2f32d7f1120f1c60db6e7aaeade574dba6d80e
lsseong <lsseong@hotmail.com>
2019-07-15 21:05:31 +0800

    modeling

--- model/model.py
+++ model/model.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 import numpy as np
 
+TIME_SERIES_INPUT = "prices"
+
 
 def markov_model(features, n_forward):
     """
@@ -28,6 +30,12 @@ def linear_model(features, n_forward):
     pass
 
 
+def compute_errors(features, labels, predictions):
+    loss = tf.losses.mean_squared_error(labels, predictions)
+    rmse = tf.metrics.root_mean_squared_error(labels, predictions)
+    return loss, rmse
+
+
 def rnn_model(features, labels, mode, params):
     """
     :param features: shape [BATCH_SIZE, SEQ_LEN, 1]
@@ -36,7 +44,7 @@ def rnn_model(features, labels, mode, params):
     :return: the EstimatorSpec
     """
 
-    # select the model
+    # 1. select the model
     model_functions = {
         'markov': markov_model,
         'linear': linear_model
@@ -46,14 +54,41 @@ def rnn_model(features, labels, mode, params):
 
     n_forward = tf.shape(labels)[1]
 
-    Yout_ = model_function(features, n_forward)
+    predictions = model_function(features[TIME_SERIES_INPUT], n_forward)
 
-    # loss function
-    #loss = tf.losses.mean_squared_error(Yout_, labels)
+    # 2. loss function, training/eval ops
+    train_op = None
 
-    # optimisation operation
+    loss, rmse = compute_errors(features, labels, predictions)
 
-    return Yout_
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        # this is needed for batch normalization, but has no effect otherwise
+        update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
+        with tf.control_dependencies(update_ops):
+            # 2b. set up training operation
+            train_op = tf.contrib.layers.optimize_loss(loss,
+                                                       tf.train.get_global_step(),
+                                                       learning_rate=params['learning_rate'],
+                                                       optimizer="Adam")
+
+    # 2c. eval metric
+    eval_metric_ops = {
+        "RMSE": rmse
+    }
+
+    # 3. Create predictions
+    predictions_dict = {"predicted": predictions}
+
+    # 4. return EstimatorSpec
+    return tf.estimator.EstimatorSpec(
+        mode=mode,
+        predictions=predictions_dict,
+        loss=loss,
+        train_op=train_op,
+        eval_metric_ops=eval_metric_ops,
+        export_outputs={
+            'predictions': tf.estimator.export.PredictOutput(predictions_dict)}
+    )
 
 
 # read data and convert to needed format
@@ -71,7 +106,7 @@ def read_dataset(filename, mode, seq_length, n_forward, batch_size=512):
 
             features = tf.stack(features)
             labels = tf.stack(labels)
-            return {"prices": features}, labels
+            return {TIME_SERIES_INPUT: features}, labels
 
         # Create list of file names
         dataset = tf.data.Dataset.list_files(filename)
@@ -95,26 +130,41 @@ def read_dataset(filename, mode, seq_length, n_forward, batch_size=512):
     return _input_fn
 
 
-def sequence_regressor():
-    pass
-
-
 def train_and_evaluate(output_dir, hparams):
     # ensure file writer cache is clear for TensorBoard events file
     tf.summary.FileWriterCache.clear()
 
+    # parameters
+    seq_length = hparams['seq_length']
+
     # training data reader
     get_train = read_dataset(hparams['train_data_path'],
                              tf.estimator.ModeKeys.TRAIN,
-                             hparams['seq_length'],
+                             seq_length,
                              hparams['n_forward'])
 
     # evaluation data reader
-    #get_valid = read_dataset(hparams['eval_data_path'], tf.estimator.ModeKeys.EVAL, 1000)
+    get_valid = read_dataset(hparams['eval_data_path'],
+                             tf.estimator.ModeKeys.EVAL,
+                             seq_length,
+                             hparams['n_forward'],
+                             batch_size=1000)
 
-    estimator = None
-    train_spec = None
-    eval_spec = None
+    estimator = tf.estimator.Estimator(model_fn=rnn_model,
+                                       params=hparams,
+                                       config=tf.estimator.RunConfig(save_checkpoints_secs=hparams['min_eval_frequency']),
+                                       model_dir=output_dir)
+
+    train_spec = tf.estimator.TrainSpec(input_fn=get_train, max_steps=hparams['train_steps'])
+
+    exporter = None
+
+    eval_spec = tf.estimator.EvalSpec(input_fn=get_valid,
+                                      steps=None,
+                                      exporters=exporter,
+                                      start_delay_secs=hparams['eval_delay_secs'],
+                                      throttle_secs=hparams['min_eval_frequency'])
+
     tf.estimator.train_and_evaluate(estimator, train_spec, eval_spec)
 
 
@@ -165,6 +215,7 @@ def test_read_dataset():
         print("try_out_input_function: features shape = {}".format(features['prices'].shape))
         print("try_out_input_function: labels shape = {}".format(labels.shape))
 
+
 def test_train_and_evaluate():
     seq_length = 8
     n_forward = 5
@@ -173,13 +224,17 @@ def test_train_and_evaluate():
 
     hparams = {}
     hparams['model'] = model
-    hparams['train_data_path'] = "../train_{}_{}.csv".format(seq_length, n_forward)
-    hparams['eval_data_path'] = "../eval_{}_{}.csv".format(seq_length, n_forward)
+    hparams['train_data_path'] = "../data/train_{}_{}.csv".format(seq_length, n_forward)
+    hparams['eval_data_path'] = "../data/eval_{}_{}.csv".format(seq_length, n_forward)
     hparams['seq_length'] = 8
     hparams['n_forward'] = 5
+    hparams['learning_rate'] = 0.2
+    hparams['train_steps'] = 100
+    hparams['eval_delay_secs'] = 10
+    hparams['min_eval_frequency'] = 60
 
     train_and_evaluate(out_dir_str, hparams)
 
 
 if __name__ == '__main__':
-    test_read_dataset()
+    test_train_and_evaluate()
