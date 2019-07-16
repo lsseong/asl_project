a18223edb034173a63797d291f21ecdd0325e6d6
lsseong <lsseong@hotmail.com>
2019-07-16 13:57:30 +0800

    remove test method from model

---
+++ try_read_dataset.py
@@ -0,0 +1,18 @@
+from model import model
+import tensorflow as tf
+
+
+if __name__ == '__main__':
+    seq_length = 8
+    n_forward = 5
+
+    with tf.Session() as sess:
+        fn = model.read_dataset(filename="data/train_{}_{}.csv".format(seq_length, n_forward),
+                                mode=tf.estimator.ModeKeys.TRAIN,
+                                seq_length=seq_length,
+                                n_forward=n_forward)
+
+        batch_features, batch_labels = fn()
+        features, labels = sess.run([batch_features, batch_labels])
+        print("try_out_input_function: features shape = {}".format(features['prices'].shape))
+        print("try_out_input_function: labels shape = {}".format(labels.shape))
